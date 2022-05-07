82b3fb78fcb97c166197adea64bd9f5837d89175
JiHyeon Noh <ggpji59105@naver.com>
2022-05-07 15:45:22 +0900

    first commit

---
+++ CH1.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torchvision import transforms
+import numpy as np
+from scipy.optimize import linear_sum_assignment as linear_assignment
+from sklearn.manifold import TSNE
+from matplotlib import pyplot as plt
+
+print("hello world!")
+print("hello world! two")
